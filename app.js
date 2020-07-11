8e5dd992732dfe7c4121cd7a11f8368837c8c2da
Nandha Sundaravadivel <nandhasundaravadivel@MacBook-Pro.knology.net>
2020-07-10 23:51:29 -0400

    Added handles

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
  app.get('/', function(req, res) {
      ds("/", function(error, data) {
-         res.end(data) // return {usedSize : "" , totalSize : ""}  in Byte
+         res.end(data.usedSize + ' ' + data.totalSize) // return {usedSize : "" , totalSize : ""}  in Byte
      })
  })
 
