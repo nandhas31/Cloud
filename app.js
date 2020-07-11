24845c2ae27ff90cd4cf76d3c9f9edcb531dfe4d
Nandha Sundaravadivel <nandhasundaravadivel@MacBook-Pro.knology.net>
2020-07-10 23:43:28 -0400

    Added handles

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
  const express = require('express');
  const app = express();
 
- app.get('/', function(res, req) {
+ app.get('/', function(req, res) {
      ds("/", function(error, data) {
          res.end(data) // return {usedSize : "" , totalSize : ""}  in Byte
      })
+ })
+
+ app.listen('8080', function() {
+     console.log('App Started!');
  })
